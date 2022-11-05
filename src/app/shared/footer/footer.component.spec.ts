039fcefc5318e080148d75c594bfb7ee79acc1d7
nathanbelt23UniAndes <111519973+nathanbelt23UniAndes@users.noreply.github.com>
2022-11-05 11:17:03 -0500

    caso 3:creacion de footer(verde)

---
+++ src/app/shared/footer/footer.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FooterComponent } from './footer.component';
+
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FooterComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should render text Contact us: +57 3102105253 - info@tusegundazo.com - @tusegundazo', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h5')?.textContent).toContain('Contact us: +57 3102105253 - info@tusegundazo.com - @tusegundazo');
+  });
+});
